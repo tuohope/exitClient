2906a179c7bf34065dc4311ea0808599753f5a52
Tuo <tuohope@gmail.com>
2017-03-30 17:31:56 -0700

    Initial Commit

---
+++ ExitGameClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExitGameClient
+//
+//  Created by Tuo on 2017-03-30.
+//  Copyright © 2017 Exit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
